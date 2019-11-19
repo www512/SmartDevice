384ba91e0db4aee5c2a2f46cfbb7805c4db9ccfb
www512 <www512@yandex.ru>
2019-11-19 11:24:57 +0300

    SmartDevice

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,6 @@
 'use strict';
+
+//Плавный скролл
 const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
       animationTime = 400,
       framesCount = 30;
@@ -20,4 +22,41 @@ anchors.forEach(function(item) {
       }
     }, animationTime / framesCount);
   });
+});
+
+//Модальное окно
+let call = document.querySelector('.site-list__button');
+let popup = document.querySelector('.modal');
+let close = document.querySelector('.modal__close');
+let overlay = document.querySelector('.page-overlay');
+let bodyScroll = document.querySelector('body');
+
+call.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    popup.classList.add('modal__show');
+    overlay.classList.add('page-overlay__show');
+    bodyScroll.classList.add('scroll-off');
+});
+
+close.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    popup.classList.remove('modal__show');
+    overlay.classList.remove('page-overlay__show');
+    bodyScroll.classList.remove('scroll-off');
+});
+
+overlay.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    popup.classList.remove('modal__show');
+    overlay.classList.remove('page-overlay__show');
+    bodyScroll.classList.remove('scroll-off');
+});
+
+window.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+        popup.classList.remove('modal__show');
+        overlay.classList.remove('page-overlay__show');
+        bodyScroll.classList.remove('scroll-off');
+    }
 });
